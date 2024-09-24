1cd8460d6cc8ac17e79a458bc2f5e98908e4a9f0
SoundwaveEarthSpark <baranova.darya2002@gmail.com>
2024-09-24 22:03:18 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fake
+from true_math import divide as plus
+
+result1 = fake(69, 3)
+result2 = fake(3, 0)
+result3 = plus(49, 7)
+result4 = plus(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
