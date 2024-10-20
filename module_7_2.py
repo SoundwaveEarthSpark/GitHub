e015677cfbe705ece43b3498ebced82193041613
SoundwaveEarthSpark <baranova.darya2002@gmail.com>
2024-10-21 00:24:17 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,25 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+    number = 0
+    key = ''
+    bite = 0
+    file = open(file_name, 'a', encoding="utf-8")
+    for i in strings:
+        number += 1
+        bite = file.tell()
+        file.write(str(f'\n{i}'))
+        key =f'({number}:{bite})'
+        strings_positions[key] = i
+    file.close()
+    return strings_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
