0555394803d098a603e0ac5b4247b63f9d224948
SoundwaveEarthSpark <baranova.darya2002@gmail.com>
2024-10-22 22:45:38 +0300

    module_7_4.py

--- module_7_3.py
+++ module_7_3.py
@@ -1,35 +1,54 @@
-class WordsFinder:
-    def __init__(self, *args):
-        self.file_names = args
 
-    def get_all_words(self):
-        all_words = {}
-        for f in self.file_names:
-            with open(f, 'r',encoding='utf-8') as file:
-                str_file = file.read()
-                str_file = str_file.lower()
-                for p in ".,=!?:;-":
-                    str_file = str_file.replace(p, '')
-                all_words[f] = str_file.split()
-        return all_words
+#Использование %
+def team_member(name,team_num):
+    print('В команде %s участников: %s !' % (name,team_num) )
 
-    def find(self, word):
-        result = {}
-        for name, words in self.get_all_words().items():
-            if word.lower() in words:
-                result[name] = words.index(word.lower()) + 1
-        return result
-    def count(self, word):
-        result = {}
-        for name, words in self.get_all_words().items():
-            result[name] = 0
-            for i in words:
-                if word.lower() == i:
-                    result[name] += 1
+def team_all_members(*team_num):
+    print('Итого сегодня в командах участников: %s и %s !' % (team_num[0], team_num[1]))
 
-        return result
+#Использование format():
+def task_count(name,score):
+    print('Команда {} решила задач: {} !'.format(name,score))
 
-finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words()) # Все слова
-print(finder2.find('TEXT')) # 3 слово по счёту
-print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+def task_time(name, team_time):
+    print('{} решили задачи за: {} !'.format(name,team_time))
+
+#Использование f-строк:
+def task_count_team(score_1, score_2):
+    print(f'Команды решили {score_1} и {score_2} игровых задач.')
+
+def challenge_results(name1, name2, score_1, score_2,team1_time,team2_time):
+    if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+        print(f'Победа команды {name1} !')
+    elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+        print(f'Победа команды {name2} !')
+    else:
+        print("Ничья")
+
+
+def results_of_game(team1_time, team2_time, *score):
+    tasks_total = sum(score)
+    time_avg = tasks_total / (team1_time + team2_time)
+    print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.")
+
+
+name1 = 'Мастер кода'
+name2 = 'Волшебники данных'
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+
+
+team_member(name1, team1_num)
+team_all_members(team1_num, team2_num)
+
+task_count(name1, score_1)
+task_time(name2, team2_time)
+
+task_count_team(score_1, score_2)
+
+challenge_results(name1, name2, score_1, score_2, team1_time, team2_time)
+results_of_game(team1_time, team2_time,score_1,score_2)
