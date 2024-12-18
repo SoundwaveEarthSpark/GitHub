8c4e0b3d48d59f4c944a052a798f8d1e99a5c27d
SoundwaveEarthSpark <baranova.darya2002@gmail.com>
2024-12-18 17:47:08 +0300

    Delete suite_12_3.py

--- suite_12_3.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from tests_12_3 import RunnerTest, TournamentTest
-
-runnerTS = unittest.TestSuite()
-runnerTS.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))
-runnerTS.addTest(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))
-
-runer = unittest.TextTestRunner(verbosity=2)
-runer.run(runnerTS)
